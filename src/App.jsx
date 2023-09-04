17a73dfa621586f21a20cf6788cd431534b21c67
AnosVoldigoad999 <oyedejiuzaymah@gmail.com>
2023-09-04 23:54:25 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,36 @@ import './App.css'
 import { useState } from 'react'; 
 import { PiInstagramLogo } from 'react-icons/pi'
 import { RiFacebookFill, RiTwitterFill  } from 'react-icons/ri'
-import { useForm } from 'react-hook-form'
 export default function App (){
   const [email, setEmail] = useState('');
- 
+  const [emailclass, setEmailClass] = useState('')
+  const [spanmess, setSpanMess] = useState('')
+  const pattern =  /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    if(!email.match(pattern)){
+      setEmailClass('red')
+      setSpanMess('please provide a valid email address')
+    } else{
+      setEmailClass('')
+      setSpanMess('')
+    }
+  } 
   return <>
   <header><img className="logo" /></header>
   <main>
     <div className="container">
     <h1>We are launching <strong>soon!</strong></h1>
-    <form onSubmit={(e)=>e.preventDefault()}>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="email">
         Subscribe and get notified
       </label>
       <div className='form'>
-      <input required={true} minLength={4}  placeholder='example@email.com'  type="email" id="email"  />
+      <input placeholder='example@email.com' className={emailclass}  type="text" id="email" value={email} onChange={e=>setEmail(e.target.value)}  />
       <button>Notify me</button>
       </div>
     </form>
+    <span>{spanmess}</span>
     <br />
     <img className="other" />
     </div>
